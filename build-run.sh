bb0646c8e9e1ac4228031371ff321c192a847a92
Bobby Christopher <bchristopher@shopmonkey.io>
2024-02-04 19:41:26 -0500

    yep

--- build-run.sh
+++ build-run.sh
@@ -1,1 +1,1 @@
-npx local-web-server --https --cors.embedder-policy "require-corp" --cors.opener-policy "same-origin" --directory "."
+npx local-web-server --https --cors.embedder-policy "require-corp" --cors.opener-policy "same-origin" --directory "./build/web"
