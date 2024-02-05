3a741eedbeff706a4634cce027d37bdb61f1b2f1
Bobby Christopher <bchristopher@shopmonkey.io>
2024-02-04 20:01:31 -0500

    cli build failing with textures

--- build-run.sh
+++ build-run.sh
@@ -1,1 +1,6 @@
-npx local-web-server --https --cors.embedder-policy "require-corp" --cors.opener-policy "same-origin" --directory "./build/web"
+/Applications/Godot.app/Contents/MacOS/Godot --headless --verbose --export-release "Web" /build/web/index.html
+npx local-web-server --https --cors.embedder-policy "require-corp" --cors.opener-policy "same-origin" --directory "./build/web" &> /dev/null
+tell application "Arc"
+    make new tab with properties {URL:"https://www.reddit.com"}
+    activate
+end tell
