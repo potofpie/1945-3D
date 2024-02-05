06d22c089a8384e48fe8be5670018bc18d432f2b
Bobby Christopher <bchristopher@shopmonkey.io>
2024-02-04 20:31:40 -0500

    trying again

--- build-run.sh
+++ build-run.sh
@@ -1,6 +1,6 @@
-/Applications/Godot.app/Contents/MacOS/Godot --headless --verbose --export-release "Web" /build/web/index.html
-npx local-web-server --https --cors.embedder-policy "require-corp" --cors.opener-policy "same-origin" --directory "./build/web" &> /dev/null
-tell application "Arc"
-    make new tab with properties {URL:"https://www.reddit.com"}
-    activate
-end tell
+/Applications/Godot.app/Contents/MacOS/Godot --verbose --export-release  "HTML5" /build/web/index.html
+# npx local-web-server --https --cors.embedder-policy "require-corp" --cors.opener-policy "same-origin" --directory "./build/web" &> /dev/null
+# tell application "Arc"
+#     make new tab with properties {URL:"https://www.reddit.com"}
+#     activate
+# end tell
